d934141a168942d938d3eedf89c4e19376d2f89f
TheLuckyCuber999 <92718237+Donkasem55@users.noreply.github.com>
2026-04-02 10:57:00 +0800

    e

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <fstream>
 
 std::map<std::string, std::string> vartypes;
 std::string kernel = "win32";
@@ -17,18 +18,17 @@ std::map<std::string, std::string> argp(int argc, char* argv[]) {
 	std::map<std::string, std::string> out = { {"--kernel", "win32"} };
 	for (int i=0; i<argc-1; i++) {
 		if (args[i].starts_with("-")) {
-			if (args[i].starts_with("--") {
+			if (args[i].starts_with("--")) {
 				arg = args[i];
 			} else {
-				switch(args[i]) {
-					case "-I":
-						arg = "--file";
-						break;
-					case "-O":
-						arg = "--output";
-						break;
-					default:
-						arg = args[i];
+				if (args[i] == "-I") {
+					arg = "--file";
+				} else if (args[i] == "-O") {
+					arg = "--output";
+				} else if (args[i] == "-S") {
+					arg = "--stdlib";
+				} else {
+					arg = args[i];
 				}
 			}
 		} else {
@@ -38,7 +38,37 @@ std::map<std::string, std::string> argp(int argc, char* argv[]) {
 	return out;
 }
 
+std::string read(std::string name) {
+	std::ifstream f(name);
+	std::string out;
+	if (!f.is_open()) {
+		std::cerr << "Error: File cannot be opened" << std::endl;
+	} else {
+		std::string l;
+		while (std::getline(f, l)) {
+			out.append(l);
+			out.append("\n");
+		}
+	}
+	f.close();
+	return out;
+}
+
+std::string istdlib(std::string lib, std::string stdlib, std::string ker="win32") {
+	std::string loc = stdlib;
+	loc.append("/");
+	loc.append(ker);
+	loc.append("/");
+	loc.append(lib);
+	std::string out = read(loc);
+	return out;
+}
+
+std::map<std::string, std::string> mac = {};
+
 int main(int argc, char *argv[]) {
 	argd = argp(argc, argv);
-	std::cout << argd["--file"];
+	std::string dat = istdlib("stdout.asm", argd["--stdlib"]);
+	std::cout << dat;
+	return 0;
 }
